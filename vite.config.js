6954288c5aded0c240c2e1fb9fc0a42bed4c52d5
vinayakkakkan <vinayakakkan@gmail.com>
2026-02-26 12:57:26 +0530

    Update base path to match repo name GPTC-kannur

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/Gptc/' : '/',
+  base: command === 'build' ? '/GPTC-kannur/' : '/',
   server: {
     port: 3000,
     open: true
